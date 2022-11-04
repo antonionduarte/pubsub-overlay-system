809b2d2e2569acac1c52bafb9eb565b5845c6c2a
daro.almeida <daro.almeida@campus.fct.unl.pt>
2022-11-04 15:37:28 +0000

    changed reliability graph x-axis to time (s)

--- analysis/scripts/metrics-structured.py
+++ analysis/scripts/metrics-structured.py
@@ -4,9 +4,9 @@ import numpy as np
 from collections import defaultdict as dd
 import matplotlib.pyplot as plt
 
-METRICS_PATH = '../metrics/'
-PLOTS_OUT_PATH = '../plots/'
-TEXT_OUT_PATH = '../text/'
+METRICS_PATH = 'metrics/'
+PLOTS_OUT_PATH = 'plots/'
+TEXT_OUT_PATH = 'text/'
 
 
 def calc_redundancy(metrics):
@@ -40,9 +40,12 @@ def create_conjoined_metrics_file(lines):
 def calc_reliability(list_node_metrics):
     all_sorted_by_time = sorted([e for m in list_node_metrics for e in m], key=lambda x: x["timestamp"])
     create_conjoined_metrics_file(all_sorted_by_time)  # for debug
-    rel_per_msg = []
+    rel_per_second = []
+    second_start = None
+    s = -1
 
     total_recv_pubs, total_expected_pubs = 0, 0
+    second_recv_pubs, second_expected_pubs = 0, 0
     subs = dd(lambda: 0)
     i = 0
     for e in all_sorted_by_time:
@@ -51,32 +54,45 @@ def calc_reliability(list_node_metrics):
         elif e["type"] == "unsubscribedTopic":
             subs[e["message"]["topic"]] -= 1
         elif e["type"] == "pubSent":
+            if second_start is None or e["timestamp"] >= second_start + 1000:
+                if second_start is not None:
+                    rel_per_second.append(
+                        (second_recv_pubs / second_expected_pubs) * 100 if second_expected_pubs > 0 else 100)
+                second_start = e["timestamp"]
+                second_recv_pubs, second_expected_pubs = 0, 0
+                s += 1
             recv_pubs = len(list(filter(
                 lambda x: x["type"] == "pubReceived" and x["message"]["delivered"] and x["message"]["messageId"] ==
-                          e["message"]["messageId"], all_sorted_by_time))) + (1 if e["message"]["delivered"] else 0)
+                          e["message"]["messageId"], all_sorted_by_time[max(0, i - 100)::]))) + (
+                            1 if e["message"]["delivered"] else 0)
             expected_pubs = subs[e["message"]["topic"]]
-            rel_per_msg.append((recv_pubs / expected_pubs) * 100 if expected_pubs > 0 else 100)
+            second_recv_pubs += recv_pubs
+            second_expected_pubs += expected_pubs
             total_recv_pubs += recv_pubs
             total_expected_pubs += expected_pubs
         i += 1
 
-    return total_recv_pubs, total_expected_pubs, rel_per_msg
+    return total_recv_pubs, total_expected_pubs, rel_per_second
 
 
-def print_reliability_results(recv_pubs, expected_pubs, rel_per_msg):
+def print_reliability_results(recv_pubs, expected_pubs, rel_per_second):
     print("Avg reliability of delivered messages: %s, (received: %d, expected: %d)" % (
         "{0:.2f}%".format(100 if expected_pubs == 0 else (recv_pubs / expected_pubs) * 100), recv_pubs, expected_pubs))
 
     fig, ax = plt.subplots(num=1, clear=True)
     os.makedirs(PLOTS_OUT_PATH, exist_ok=True)
-    ax.plot(rel_per_msg, label="GossipSub+Kademlia", color="blue")
-    ax.set(xlabel='Messages', ylabel='Avg Reliability (%)', xlim=(1, len(rel_per_msg)), ylim=(0, 105))
+    ax.plot(rel_per_second, label="GossipSub+Kademlia", color="blue")
+    ax.set(xlabel='Time (s)', ylabel='Avg Reliability (%)', xlim=(1, len(rel_per_second)-1), ylim=(0, 105))
     ax.legend()
     fig.tight_layout()
     fig.savefig(PLOTS_OUT_PATH + "GossipSub_Kademlia.pdf")
+    print(rel_per_second)
 
 
 if __name__ == "__main__":
+    os.makedirs(PLOTS_OUT_PATH, exist_ok=True)
+    os.makedirs(TEXT_OUT_PATH, exist_ok=True)
+
     sum_received, sum_not_delivered = 0, 0
     list_avg_hops = []
     list_node_metrics = []
@@ -102,5 +118,5 @@ if __name__ == "__main__":
     print("Overall:")
     print_redundancy_results(sum_received, sum_not_delivered)
     print_hop_latency_results(np.mean(list_avg_hops))
-    recv_pubs, expected_pubs, rel_per_msg = calc_reliability(list_node_metrics)
-    print_reliability_results(recv_pubs, expected_pubs, rel_per_msg)
+    recv_pubs, expected_pubs, rel_per_second = calc_reliability(list_node_metrics)
+    print_reliability_results(recv_pubs, expected_pubs, rel_per_second)
